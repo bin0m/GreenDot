959931445bb9a35f10ba332ba074bb167f1a0c67
Gregory Levit <levitg@gmail.com>
2022-02-02 01:15:39 +0500

    Add Repoistory and db context

--- GreenDot.API/Startup.cs
+++ GreenDot.API/Startup.cs
@@ -11,6 +11,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GreenDot.API.DbContexts;
+using GreenDot.API.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace GreenDot.API
 {
@@ -26,12 +29,19 @@ namespace GreenDot.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GreenDot.API", Version = "v1" });
             });
+
+            services
+                .AddScoped<ICourseLibraryRepository, CourseLibraryRepository>()
+                .AddDbContext<CourseLibraryContext>(options =>
+                {
+                    options.UseSqlServer(
+                        @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
