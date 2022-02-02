7895fb8404f4c56313543cfeb8aff3d18ab92695
Gregory Levit <levitg@gmail.com>
2022-02-02 14:40:12 +0500

    Get author by id

--- GreenDot.API/Controllers/AuthorsController.cs
+++ GreenDot.API/Controllers/AuthorsController.cs
@@ -1,25 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using GreenDot.API.Entities;
 using GreenDot.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GreenDot.API.Controllers
 {
     [ApiController]
+    [Route("api/authors")]
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
-
+            
         public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
         {
             _courseLibraryRepository = courseLibraryRepository ??
                                        throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
 
-        [HttpGet("api/authors")]
+        [HttpGet]
         public IActionResult GetAuthors()
         {
-            var authors = _courseLibraryRepository.GetAuthors();
-            return new JsonResult(authors);
-        }        
+            IEnumerable<Author> authors = _courseLibraryRepository.GetAuthors();
+            return Ok(authors);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetAuthor(Guid id)
+        {
+            Author author = _courseLibraryRepository.GetAuthor(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author);
+        }
+
     }
 }
