8a3cd3837f5ea76d15d4b154e95c86be4167e11b
Gregory Levit <levitg@gmail.com>
2022-02-03 00:40:08 +0500

    Add GET courses for Author

---
+++ GreenDot.API/Controllers/CoursesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using GreenDot.API.Models;
+using GreenDot.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDot.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors/{id}/courses")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                                       throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ??
+                      throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CourseDto>> GetCoursesForAuthor(Guid id)
+        {
+            if (!_courseLibraryRepository.AuthorExists(id))
+            {
+                return NotFound(); 
+            }
+            var courses = _courseLibraryRepository.GetCourses(id);
+            var coursesDto = _mapper.Map<IEnumerable<CourseDto>>(courses);
+
+            return Ok(coursesDto);
+        }
+                
+    }
+}
---
+++ GreenDot.API/MapperProfiles/CoursesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GreenDot.API.Entities;
+using GreenDot.API.Models;
+
+namespace GreenDot.API.MapperProfiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Course, CourseDto>();
+        }
+    }
+}
