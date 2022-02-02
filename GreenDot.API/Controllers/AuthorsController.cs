3905f784d6140dbb42686f4e2b5fdb6daffa84b9
Gregory Levit <levitg@gmail.com>
2022-02-02 13:08:20 +0500

    Add authors controller

---
+++ GreenDot.API/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GreenDot.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDot.API.Controllers
+{
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                                       throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authors = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authors);
+        }        
+    }
+}
