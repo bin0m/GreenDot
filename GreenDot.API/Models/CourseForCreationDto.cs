1a143b6283f6c59c817906b0a86276171de7be79
Gregory Levit <levitg@gmail.com>
2022-02-28 23:41:04 +0300

    Add Course validation

--- GreenDot.API/Models/CourseForCreationDto.cs
+++ GreenDot.API/Models/CourseForCreationDto.cs
@@ -1,9 +1,24 @@
-﻿namespace GreenDot.API.Models
-{
-    public class CourseForCreationDto
-    {
-        public string Title { get; set; }
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
+namespace GreenDot.API.Models
+{
+    public class CourseForCreationDto : IValidatableObject
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        
+        [MaxLength(1500)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult("The provided description should be different from title.",
+                    new[] {"CourseForCreationDto"});
+            }
+        }
     }
 }
